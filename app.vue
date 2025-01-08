32fd780812b7d0bdaf937058d2041e61dc873774
maamokun/MikanDev <maamo-kun@outlook.com>
2025-01-09 00:40:47 +0900

    fix: version

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <Toaster rich-colors/>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Toaster } from 'vue-sonner'
+</script>
